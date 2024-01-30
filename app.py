ef87cac5893988ee646f6eaee60e1b42e2d6a808
ChoiJinHyeok <chlwlsgur000@naver.com>
2024-01-30 11:41:58 +0900

    dfas

--- app.py
+++ app.py
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import streamlit as st
 import seaborn as sns
 
-@data_cache
+@st.cache_data
 def load_data():
-    data = sns.load_dataset("iris")
-    return data
+    df = sns.load_dataset("iris")
+    return df
+
 def main():
-    st.title("Hello world on strealit.io")
+    st.title("Hello World on Streamlit.io")
+    iris = load_data()
+    st.table(iris)
 
 if __name__ == "__main__":
-=======
-# -*- coding: utf-8 -*-
-
-import streamlit as st
-import seaborn as sns
-
-@data_cache
-def load_data():
-    data = sns.load_dataset("iris")
-    return data
-def main():
-    st.title("Hello world on strealit.io")
-
-if __name__ == "__main__":
->>>>>>> f42a5da28ceb3dad3b0666ad59bc6a69587e2ee1
     main()
